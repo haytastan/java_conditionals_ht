d950dc0fd3aa867532c124c236f67858809042d2
hayyam <haytastan@gmail.com>
2022-10-02 17:57:10 +0300

    Conditionals (if, switch-case) in java language v.20 (by Dr Hayati TASTAN)
    Signed-off-by: hayyam <haytastan@gmail.com>

--- src/com/hayatitastan/Main.java
+++ src/com/hayatitastan/Main.java
@@ -5,19 +5,36 @@ package com.hayatitastan;
 public class Main {
     public static void main(String[] args) {
 
-        //int sayi = 1918;
-        //int sayi = 1923;
-        int sayi = 1962;
-        //int sayi = 2024;
+        // if blocks
 
-        if(sayi<1920) {
-            System.out.println("Henüz Türkiye Cumhuriyeti kurulmadı");
-        }else if (sayi==1920 || sayi==1923) {
-            System.out.println("Türkiye Cumhuriyeti kuruldu");
-        }else if (sayi>1923 && sayi<=2023 ) {
-            System.out.println("Türkiye Cumhuriyeti'nin ilk yüzyılı içindeyiz (1923-2023)");
-        }else {
-            System.out.println("\"Türkiye Cumhuriyeti'nin ikinci yüzyılı içindeyiz (2023 sonrası)");
+        // find the largest number
+        int sayi1 = 20;
+        int sayi2 = 25;
+        int sayi3 = 28;
+        int enBuyuk = sayi1;
+
+        if (enBuyuk<sayi2){
+            enBuyuk = sayi2;
+        }
+        if (enBuyuk<sayi3){
+            enBuyuk=sayi3;
+        }
+        System.out.println("En Büyük Sayı = " + enBuyuk);
+
+        // switch blocks
+        char grade = 'K';
+        switch (grade) {
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+                System.out.println("Geçtiniz");
+                break;
+            case 'F':
+                System.out.println("Kaldınız");
+                break;
+            default:
+                System.out.println("Geçersiz not girdiniz");
         }
 
     }
